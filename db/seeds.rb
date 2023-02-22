547c90b0a649e88e3cd5321538148bf923893f6c
Caroline MAURICE <caroline.maurice.henry@gmail.com>
2023-02-22 08:56:44 +0100

    rails wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  faker_character = Faker::Movies::StarWars.character
+  faker_content = Faker::Movies::StarWars.quote
+  Article.create(title: faker_character, content: faker_content)
+end
